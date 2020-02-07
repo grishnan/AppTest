13a54de43251ed503b6a38647b41a503bf981217
Grigoriy Zhdanov <grishnan@gmail.com>
2020-02-07 06:55:13 +0400

    Добавление кода в процедуру Button1_Click

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace AppTest
 {
@@ -14,6 +15,22 @@ namespace AppTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Администратор\Source\Repos\AppTest\database.mdf;Integrated Security=True;Connect Timeout=30";
+            String queryString = "SELECT * FROM Surnames";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(queryString, conn); // создаём объект запроса
+                conn.Open(); // открываем соединение с БД
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}", reader[0]); // Вывод должен быть в TextEdit
+                }
+            }   
         }
     }
 }
